ad6007450169d32816bb8af26cdd3f966e8507fd
sharpbai <sharpbai@gmail.com>
2019-11-20 22:21:22 +0800

    fix(compile): fix compile on macos

--- compile_osx.sh
+++ compile_osx.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-gcc -dynamiclib -o build/liba.dylib -flat_namespace printa.c liba.c
-gcc -dynamiclib -o build/libe.dylib -flat_namespace printe.c libe.c
+gcc -dynamiclib -o liba.dylib -flat_namespace printa.c liba.c
+mv liba.dylib build/
+gcc -dynamiclib -o libe.dylib -flat_namespace printe.c libe.c
+mv libe.dylib build/
 
-gcc -dynamiclib -o build/liba_nf.dylib liba.c
-gcc -dynamiclib -o build/libe_nf.dylib libe.c
+gcc -dynamiclib -o liba_nf.dylib printa.c liba.c
+mv liba_nf.dylib build/
+gcc -dynamiclib -o libe_nf.dylib printe.c libe.c
+mv libe_nf.dylib build/
 
 gcc -o build/mainE main.c -L./build -le -la
 gcc -o build/mainA main.c -L./build -la -le
