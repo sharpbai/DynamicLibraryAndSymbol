2eeb329400a9d6c57c45cfee6da2f282504e5ab1
sharpbai <tian.bai@duobei.com>
2019-11-20 21:46:41 +0800

    fix(compile): fix compile script onlinux

--- compile_linux.sh
+++ compile_linux.sh
@@ -3,8 +3,8 @@
 gcc -shared -fPIC -o build/liba.so printa.c liba.c
 gcc -shared -fPIC -o build/libe.so printe.c libe.c
 
-gcc -shared -fPIC -fvisibility=hidden -o build/liba_nf.so liba.c
-gcc -shared -fPIC -fvisibility=hidden -o build/libe_nf.so libe.c
+gcc -shared -fPIC -fvisibility=hidden -o build/liba_nf.so printa.c liba.c
+gcc -shared -fPIC -fvisibility=hidden -o build/libe_nf.so printe.c libe.c
 
 gcc -o build/mainE main.c -Wl,-rpath=./ -L./build -le -la
 gcc -o build/mainA main.c -Wl,-rpath=./ -L./build -la -le
